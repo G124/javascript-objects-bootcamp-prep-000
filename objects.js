e1dcf011f515fb791fe999d6db9abca7684e4409
Gavriel Hass <gavhass@gmail.com>
2017-11-03 17:32:46 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,16 @@
+
+var playlist = { artistNames:'Steve',
+                  songTitle:'Love'
+};
+
+
+
+ function updatePlaylist(playlist, artistName, songTitle) {
+
+   playlist[artistName] = songTitle
+
+
+   return playlist
+
+
+};
